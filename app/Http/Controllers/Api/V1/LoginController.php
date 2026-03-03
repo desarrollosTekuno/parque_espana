39266dc0e555db49ba259ac5a284b0e6618bb53e
Antonio Toxqui Sosa <antoniotoxquisosa@hotmail.com>
2026-03-03 17:53:48 -0600

    agregar api para login de usuario móvil

---
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Inertia\Inertia;
+
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+            // 'device' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
+            ], 200);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 200);
+        }
+        return response()->json([
+            // 'token' => $request->user()->createToken($request->device)->plainTextToken,
+            // 'message' => 'Success'
+            'success' => true,
+            'message' => 'Login successful',
+            'token' => $request->user()->createToken($request->email)->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successful'
+        ]);
+    }
+}
