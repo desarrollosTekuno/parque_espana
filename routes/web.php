fc957dcd9084379f5df6c5e6c9a3f221d50dda9f
Antonio Toxqui Sosa <antoniotoxquisosa@hotmail.com>
2026-03-09 16:33:19 -0600

    se agregaron funciones para verificación de correo

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
 use App\Http\Controllers\Web\DashboardController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect(route('login'));
 });
+
+Route::get('/email/verify', function () {
+    return Inertia::render('Auth/VerifyEmail');
+})->middleware('auth')->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect()->route('dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('status', 'verification-link-sent');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::middleware([
     'auth:sanctum',
