4e1af660941057d04a9925dd96416b32747f35db
ramsss-diego <ramosss.diego27@gmail..com>
2026-03-10 17:52:31 -0600

    Se agregan avances para vista reservaciones

--- app/Models/AdminClub/Reservation.php
+++ app/Models/AdminClub/Reservation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\AdminClub;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +12,13 @@ class Reservation extends Model {
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
     protected $dates = ['deleted_at'];
+
+    public function amenity()
+    {
+        return $this->belongsTo(Amenity::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
