f2ac6355bf677392f0b66b8abbc87a7bccd32a0b
Antonio Toxqui Sosa <antoniotoxquisosa@hotmail.com>
2026-03-10 16:50:25 -0600

    interceptar errores en api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,14 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
-
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -25,7 +27,39 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionWithContextMiddleware::class,
         ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Unauthenticated'
+                ], 401);
+            }
+
+        });
+        // Sin permisos
+        $exceptions->render(function (AuthorizationException $e, $request) {
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Forbidden'
+                ], 403);
+            }
+
+        });
+        // Ruta no encontrada
+        $exceptions->render(function (NotFoundHttpException $e, $request) {
+
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Endpoint not found'
+                ], 404);
+            }
+
+        });
     })
     ->create();
