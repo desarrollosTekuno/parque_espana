18382c9df8215d1459eaa1713fe84832e15389b2
ramsss-diego <ramosss.diego27@gmail.com>
2026-03-13 18:08:18 -0600

    Se agregan avances para modulo variables del sistema

--- routes/adminclubs.php
+++ routes/adminclubs.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Web\AdminClub\AmenityController;
 use App\Http\Controllers\Web\AdminClub\ReservationController;
+use App\Http\Controllers\Web\AdminClub\SystemVariableController;
 use App\Http\Controllers\Web\Administrator\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/amenities', AmenityController::class)->names('amenities');
 
 Route::resource('/reservations', ReservationController::class)->only(['index', 'update'])->names('reservations');
+
+Route::resource('/system-variables', SystemVariableController::class)->only(['index'])->names('system-variables');
