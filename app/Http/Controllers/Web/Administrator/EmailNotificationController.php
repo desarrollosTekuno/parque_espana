d44fbbac9581dac4117679e24c27a91582c72cff
Osiris <Usir95.hp@gmail.com>
2026-05-13 18:34:22 -0600

    Archivo faltante

--- app/Http/Controllers/Web/Administrator/EmailNotificationController.php
+++ app/Http/Controllers/Web/Administrator/EmailNotificationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Web\Administrator;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Inertia\Inertia;
 
 class EmailNotificationController extends Controller
